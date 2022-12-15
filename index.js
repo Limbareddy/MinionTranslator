3ddc75fe93c86e494332eca4c65cf12d5f48e9d5
Limbareddy <97155209+Limbareddy@users.noreply.github.com>
2022-12-15 11:32:14 +0530

    taking input and console

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var btnClick =document.querySelector("#btn-click");
+var txtinput=document.querySelector("#txt");
+
 btnClick.addEventListener("click",function clickHandler(){
 console.log( 'https://www.google.co.in/')
+console.log(txtinput.value)
 
 })
 
